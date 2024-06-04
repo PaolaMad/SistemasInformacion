0e43e07ea66a7b4000be6522c2ab913b4a8a4f4c
PaolaMad <paolamadridt@gmail.com>
2024-06-03 21:55:49 -0600

    clase

---
+++ src/screens/HelloWorldScreen.tsx
@@ -0,0 +1,7 @@
+import {Text, View} from 'react-native';
+export const HelloWorldScreen = () => {
+    return (
+    <View style={ {flex: 1, backgroundColor: 'red'} }>
+        <Text>Hello World Screen</Text>
+    </View>);
+};
